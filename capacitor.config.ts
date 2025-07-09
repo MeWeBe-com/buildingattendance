0f9f4e57a6be6894209c89847d88b05fd1221e96
Najam-Us-Saqib <Saqib92@users.noreply.github.com>
2025-07-09 18:19:43 +0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'cbre',
+  webDir: 'www'
+};
+
+export default config;
