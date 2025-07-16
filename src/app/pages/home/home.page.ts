2a5429527c36e5a843c6a8ebad55be8b72672cf6
Najam-Us-Saqib <Saqib92@users.noreply.github.com>
2025-07-16 16:51:03 +0500

    home page UI updates

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { IonContent, IonToggle, IonText, IonIcon, IonCheckbox } from '@ionic/angular/standalone';
+import { IonContent, IonToggle, IonIcon, IonCheckbox } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonContent, IonToggle, IonText, IonIcon, IonCheckbox],
+  imports: [IonContent, IonToggle, IonIcon, IonCheckbox],
 })
 export class HomePage {
   constructor() { }
