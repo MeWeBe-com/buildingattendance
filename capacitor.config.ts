9e0d4c2e86bf76b0f45669f199550db4f4237863
Najam-Us-Saqib <Saqib92@users.noreply.github.com>
2025-07-24 15:22:58 +0500

    firebase added, ios plttform added,

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'CBRE',
+  appId: 'com.cbrm.app',
+  appName: 'CBRM',
   webDir: 'www',
   plugins: {
     Keyboard: {
