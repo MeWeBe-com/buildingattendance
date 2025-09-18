f44a652d848358478c48634e85ba0da52e90c2f5
Najam-Us-Saqib <Saqib92@users.noreply.github.com>
2025-09-18 20:48:00 +0500

    App rename

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import { KeyboardResize } from '@capacitor/keyboard';
 
 const config: CapacitorConfig = {
   appId: 'com.qtcbresentry.app',
-  appName: 'Sentry QT',
+  appName: 'Cocoon live',
   webDir: 'www',
   plugins: {
     Keyboard: {
