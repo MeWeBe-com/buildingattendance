17ffb917254eb35a055cf36762c1aba1e44751d4
Najam-Us-Saqib <Saqib92@users.noreply.github.com>
2025-12-26 16:27:24 +0500

    changes

--- src/app/providers/authguard.service.ts
+++ src/app/providers/authguard.service.ts
@@ -39,39 +39,41 @@ export class PermissionsService {
     }
 
     return new Promise(async (resolve, reject) => {
-      let loginToken = await this.storage.getObject('login_token');
-      if (loginToken) {
-        GlobaldataService.loginToken = loginToken;
-        this.http.get('GetUserDetails', false).subscribe({
-          next: async (res2: any) => {
-            if (res2.status == true) {
-              GlobaldataService.userObject = res2.data;
-              await this.storage.setObject('CBREuserObject', res2.data);
-              await this.analytics.setUserId(res2.data.user_id)
-              await this.analytics.logEvent('app_open', { user_id: res2.data.user_id })
-              this.isLoggedIn = true;
-              if (res2.data.is_checked_in == true) {
-                this.events.publishIsCheckedIn(true);
-                this.general.goToRoot('checkout')
-              } else {
-                this.general.goToRoot('home')
-                resolve(true);
-              }
-            } else {
-              await this.storage.clear()
-              reject(true)
-            }
-          },
-          error: async (err) => {
-            await this.storage.clear()
-            reject(true);
-            this.general.goToPage('login')
-          }
-        })
-      } else {
-        reject(false);
-        this.general.goToPage('login')
-      }
+      // let loginToken = await this.storage.getObject('login_token');
+      // if (loginToken) {
+      //   GlobaldataService.loginToken = loginToken;
+      //   this.http.get('GetUserDetails', false).subscribe({
+      //     next: async (res2: any) => {
+      //       if (res2.status == true) {
+      //         GlobaldataService.userObject = res2.data;
+      //         await this.storage.setObject('CBREuserObject', res2.data);
+      //         await this.analytics.setUserId(res2.data.user_id)
+      //         await this.analytics.logEvent('app_open', { user_id: res2.data.user_id })
+      //         this.isLoggedIn = true;
+      //         if (res2.data.is_checked_in == true) {
+      //           this.events.publishIsCheckedIn(true);
+      //           this.general.goToRoot('checkout')
+      //         } else {
+      //           this.general.goToRoot('home')
+      //           resolve(true);
+      //         }
+      //       } else {
+      //         await this.storage.clear()
+      //         reject(true)
+      //       }
+      //     },
+      //     error: async (err) => {
+      //       await this.storage.clear()
+      //       reject(true);
+      //       this.general.goToPage('login')
+      //     }
+      //   })
+      // } else {
+      //   reject(false);
+      //   this.general.goToPage('login')
+      // }
+
+      resolve(true)
 
     });
   }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,14 +1,14 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://cbrm.qtcloud.co.uk/api/',
-  baseUrl2: 'https://cbrm.qtcloud.co.uk/web/',
-  firebaseConfig: {
-    apiKey: "AIzaSyDI3phMvdIvEyRUJC_6kRCCiS1s8epP02Q",
-    authDomain: "cbre-sentry-4a111.firebaseapp.com",
-    projectId: "cbre-sentry-4a111",
-    storageBucket: "cbre-sentry-4a111.firebasestorage.app",
-    messagingSenderId: "331181891382",
-    appId: "1:331181891382:web:2384b1bc2ec0a1a5d65d97",
-    measurementId: "G-Q0JMCY7VR1"
+  baseUrl: '',
+  baseUrl2: '',
+  firebaseConfig: {    
+    apiKey: "AIzaSyDI9phMvdivEzRUJC_6kRRTiS1s8epP08Q",
+    authDomain: "atten-senddtry-4a555.firebaseapp.com",
+    projectId: "atten-senddtry-4a555",
+    storageBucket: "atten-senddtry-4a555firebasestorage.app",
+    messagingSenderId: "33119782191382",
+    appId: "1:33119782191382:web:2384c1bd2ec1a1a5d56d97",
+    measurementId: "G-Q0TYMY7VE1"
   }
 };
